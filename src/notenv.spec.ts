3098460fc2b1fcf590c2863f4292cfdbe9811365
Flaque <evanjamesconrad@gmail.com>
2019-10-25 13:09:48 -0700

    Add tests

---
+++ src/notenv.spec.ts
@@ -0,0 +1,29 @@
+import proclaim from ".";
+
+test("proclaim can get a development secret", () => {
+  const env = proclaim({
+    MY_SECRET: "dummy"
+  });
+
+  process.env.NODE_ENV = "development";
+  expect(env("MY_SECRET")).toBe("dummy");
+});
+
+test("env will crash in production if the secret doesn't exist", () => {
+  const env = proclaim({
+    MY_SECRET: "dummy"
+  });
+
+  process.env.NODE_ENV = "production";
+  expect(() => env("MY_SECRET")).toThrow();
+});
+
+test("env will NOT crash in production if the secret DOES exist", () => {
+  const env = proclaim({
+    MY_SECRET: "dummy"
+  });
+
+  process.env.NODE_ENV = "production";
+  process.env.MY_SECRET = "prod-secret";
+  expect(env("MY_SECRET")).toBe("prod-secret");
+});
