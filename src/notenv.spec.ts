0d940a8734c0ae1870cc0e445e4da13a4762afaf
Flaque <evanjamesconrad@gmail.com>
2019-10-25 13:11:05 -0700

    Add 'env will take the development secret if not in production' test

--- src/notenv.spec.ts
+++ src/notenv.spec.ts
@@ -27,3 +27,13 @@ test("env will NOT crash in production if the secret DOES exist", () => {
   process.env.MY_SECRET = "prod-secret";
   expect(env("MY_SECRET")).toBe("prod-secret");
 });
+
+test("env will take the development secret if not in production", () => {
+  const env = proclaim({
+    MY_SECRET: "dummy"
+  });
+
+  process.env.NODE_ENV = "development";
+  process.env.MY_SECRET = "prod-secret";
+  expect(env("MY_SECRET")).toBe("dummy");
+});
